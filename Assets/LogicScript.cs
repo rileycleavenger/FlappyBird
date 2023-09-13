64bb205214be1d776431fcf6a8ce35b8d12fe0c2
rileycleavenger <riley.cleavenger@ufl.edu>
2023-09-13 00:34:00 -0400

    score works

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -8,10 +8,16 @@ public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public TextMeshProUGUI scoreText;
+    public bool firstTime;
 
     [ContextMenu("Add Score")]
     public void addScore(int scoreToAdd){
+        if(!firstTime){
+            firstTime = true;
+            playerScore = 0;
+        }
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
+
 }
